0ef497f153aa8ef6781b3b7e8c6d0ea90906c283
themuhammadsami <94763633+themuhammadsami@users.noreply.github.com>
2025-07-29 18:58:09 +0200

    Update install_mt5.sh

--- install_mt5.sh
+++ install_mt5.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Downloading MetaTrader 5 setup..."
-wget https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe
+echo "Installing dependencies..."
+sudo apt update && sudo apt install -y xvfb wine64 wine32 unzip wget cabextract winbind
 
-echo "Running MT5 installer with Wine (headless)"
-xvfb-run wine mt5setup.exe
+echo "Downloading MetaTrader 5 setup..."
+wget -O mt5setup.exe https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe
+
+echo "Launching MT5 installer with virtual display (xvfb)..."
+xvfb-run -a wine mt5setup.exe
+
+echo "Installation attempted. If installer froze, please upload terminal64.exe manually."
