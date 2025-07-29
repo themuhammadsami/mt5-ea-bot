191d2cdbe490d70d9e1ba9a15644ff737fb602a8
themuhammadsami <94763633+themuhammadsami@users.noreply.github.com>
2025-07-29 18:49:18 +0200

    Update install_mt5.sh

--- install_mt5.sh
+++ install_mt5.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-set -e
-export DISPLAY=:1
+echo "Downloading MetaTrader 5 setup..."
+wget https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe
 
-# Create virtual display in background
-Xvfb :1 &
-
-# Download MetaTrader 5 setup
-wget -O mt5setup.exe "https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe"
-
-# Run setup with wine
-wine mt5setup.exe || echo "Ignore GUI errors, install still proceeds"
+echo "Running MT5 installer with Wine (headless)"
+xvfb-run wine mt5setup.exe
